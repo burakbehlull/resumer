861388829d6f52e38b111af5ec27379c2937e522
burakbehlull <burakyabgu@gmail.com>
2024-03-23 21:44:49 +0300

    feat: Add font and skeletal structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
-
+import './main.scss'
 function App() {
   return (
     <>
-      <h1>Merhaba Resumer!</h1>
+      <header>
+        <nav>
+          <ul>
+            <li>RESUMER</li>
+            <li>Profile Name</li>
+            <li>Profile</li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <h1>Merhaba</h1>
+      </main>
     </>
   )
 }
