2bf15d3ec8e954f12f9cfe524712f175d88813bf
burakbehlull <burakyabgu@gmail.com>
2024-04-30 21:33:25 +0300

    feat: Complete register function

--- server/src/controllers/userControllers.ts
+++ server/src/controllers/userControllers.ts
@@ -1,8 +1,34 @@
 import {Request, Response} from 'express'
+import User from '../models/User.js'
 function index(req: Request, res: Response){
     res.json({})
 }
 
+async function register(req:Request, res: Response){
+    try {
+        const {displayName, username, email, password} = req.body
+        const user = await User.findOne({email: email})
+        const usernameControl = await User.findOne({username: username})
+        if(user || usernameControl) {
+            res.json({
+                message: 'Böyle bir kullanıcı var.'
+            })   
+        }
+        await User.create({displayName, username, email, password})
+        res.json({
+            success: true,
+            message: 'Kullanıcı yaratıldı'
+        })
+
+    } catch (err) {
+        return {
+           success: false,
+           error: err 
+        }
+    }
+}
+
 export {
-    index
+    index,
+    register
 }
--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,23 +1,38 @@
 import { Schema, model, Document } from "mongoose"
 
 interface IUser extends Document {
+    displayName: string,
     username: string,
     email: string,
-    password: string
+    password: string,
+    createdAt: Date
 }
 
 const userSchema : Schema = new Schema({
+    displayName: {
+        type: String,
+        unique: true,
+        required: true
+    },
     username: {
         type: String,
-        required: true
+        unique: true,
+        trim: true,
+        required: true,
+
     },
     email: {
         type: String,
+        unique: true,
         required: true
     },
     password: {
         type: String,
         required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
     }
 })
 
