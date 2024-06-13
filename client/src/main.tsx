d16760d38e54b8225dba38cfdae6e2b351eab833
burakbehlull <burakyabgu@gmail.com>
2024-06-13 22:53:05 +0300

    feat: Create store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,16 @@
 import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
 import Routes from './routes/index'
+import {store} from './store/index'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Routes />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
